46cdc77c0f5b1847c857cd543f9f3b349e75fc66
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2019-04-05 09:37:46 +0700

    Adding js to change carousel class behaviour

--- frontend/web/js/site.js
+++ frontend/web/js/site.js
@@ -1,2 +1,19 @@
-var img = Array.from(document.getElementsByTagName("img"));
-console.log(img);
+var img = document.getElementsByTagName("img");
+img[2].parentElement.parentElement.setAttribute("class", "item active");
+
+// var imgArr = Array.from(img);
+// console.log(img);
+
+// var filterImg;
+
+// console.log(img[2].parentElement.parentElement.classList("active"));
+
+// imgArr.forEach(i => {
+//   var src = i.getAttribute("src");
+//   if (src.includes("8_title")) {
+//     filterImg = i;
+//   }
+//   console.log(src);
+// });
+
+// console.log(filterImg);
